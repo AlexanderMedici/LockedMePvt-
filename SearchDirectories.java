720e2ce90bdeecf1b04b95dc134a7cd8015a1e1b
Alexander Medici <contactimedici@gmail.com>
2022-09-06 20:48:13 +0400

    update

--- SearchDirectories.java
+++ SearchDirectories.java
@@ -1,9 +1,11 @@
 package lockedMePvt;
+
 import java.io.File;
 import java.util.Scanner;
+import java.util.Arrays;
 
-
-public class SearchDirectories {
+public class SearchDirectories{
+	
 void searchingafile(){
 	String path ="D:\\FULL STACK SOFTWARE ENGINEER LIBRARY\\CALTECH PROJECTS\\";
 	try (Scanner Scnr = new Scanner(System.in)) {
@@ -12,7 +14,10 @@ void searchingafile(){
  
 		File paper = new File(path);  
 		int flag = 0;
+//		created an array named filled paper 
 		File filedPaper[]=paper.listFiles();
+//		making sure array is sorted. 
+		Arrays.sort(filedPaper); 
 		for(File aaa:filedPaper) {
 		if(aaa.getName().equals(filedFun))
 		{
@@ -24,14 +29,50 @@ void searchingafile(){
 		}
 
 }
+		
 
 if(flag==1) {
 		System.out.println("✅ DIRECTORY  FOUND ✅ ");
+	
+
+		
 }
 else {
 		System.out.println("🛑DIRECTORY NOT FOUND🛑");
-}
-	}
-}
+			}
 
+		}
+	
 }
+	
+
+	
+	}
+
+
+
+//public static void main(String[] args) {
+//	File fileDir = new File("D:\\\\FULL STACK SOFTWARE ENGINEER LIBRARY\\\\CALTECH PROJECTS\\");
+//
+//public static void main(String[] args) {
+//	File fileDir = new File("D:\\\\FULL STACK SOFTWARE ENGINEER LIBRARY\\\\CALTECH PROJECTS\\");
+//	if(fileDir.isDirectory()){
+//		List<String> listFile = Arrays.asList(fileDir.list());
+//		Collections.sort(listFile);
+//		System.out.println("---------------------------------------");
+//		System.out.println("Sorting by filename in ascending order");
+//		for(String s:listFile){
+//			System.out.println(s);
+//		}
+//
+//
+//	}
+//	else{
+//		System.out.println(fileDir.getAbsolutePath() + " is not a directory");
+//	}
+//
+//}
+//
+//
+//
+//}
